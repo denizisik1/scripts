b31964bb2ab3f84a609c54d5ad95fc690a505e5b
denizisik <denizkisisel1@gmail.com>
2025-08-04 21:56:40 +0300

    add shell warnings

---
+++ server/shell_warnings.sh
@@ -0,0 +1,19 @@
+# Don'u use this on your personal computer since it is not tested well, can have side effects.
+
+# Can be added to the bottom of ~/.bashrc
+# After sourcing write echo 1 to test if it works.
+
+if [[ $- == *i* ]]; then
+  trap '__mysql_warn_hook' DEBUG
+fi
+
+__mysql_warn_hook() {
+  [[ -z "$BASH_COMMAND" ]] && return
+
+  local cmd="$(echo "$BASH_COMMAND" | sed -E 's/[[:space:]]+/ /g')"
+  case "$cmd" in
+    "sudo systemctl start mysql"|"sudo systemctl stop mysql"|"echo 1")
+      echo -e "\033[1;33m[WARNING]\033[0m Don't forget to adjust max_connections"
+      ;;
+  esac
+}
